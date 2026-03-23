d89c29d182da619f3288d6a222ba8bf052db884b
hassanali-webb <hassanali2420006@gmail.com>
2026-03-23 15:40:57 +0500

    flask update-done

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Project 1 CI/CD Running Updated by Hassan"
+    return "Project 1 updateeee ho rhaaa haa"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
